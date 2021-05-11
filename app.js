296115f84438e4688af5c4bc309e3d898178fe4c
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-05-10 22:52:55 -0300

    finalização do projeto
    adicição de HTML e PDF

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var Reader = require("./Reader");
 var Processor = require("./Processor");
 var Table = require("./Table");
-
+var HtmlParser = require("./HtmlParser")
+var Writer = require("./Writer")
+var PDFWriter = require ("./PDFWriter")
 var leitor = new Reader();
+var escritor = new Writer();
 
 async function main(){ // preciso criar uma função e essa função tem que ser Async
     var dados = await leitor.Read("./users.csv")// dentro de ssa função async posso pegar o resultado da variavel e usar await e dentro dessa variavel aparece o conteudo que esta em user
@@ -14,9 +17,10 @@ async function main(){ // preciso criar uma função e essa função tem que ser
    // usuarios.rows.push(["jão jão", "formação php", "php","32"]) <= comando para adicionar linhas
     
     
-    console.log(usuarios.ColumnCount)
-    console.log(usuarios.RowCount)
+    var html = await HtmlParser.Parse(usuarios);
 
+    escritor.Write(Date.now() + ".html",html);
+    PDFWriter.WritePDF(Date.now() + ".PDF",html);
 
 }
 
